d4e9c7bab11781d549ce45b9c30e0f3a94d9b227
LuzTorres2025 <luzdeymartr@gmail.com>
2025-05-18 20:48:57 -0400

    nuevos cambios

--- src/routes/IndexRoutes.py
+++ src/routes/IndexRoutes.py
@@ -4,13 +4,15 @@ import traceback
 
 # Logger
 from src.utils.Logger import Logger
+
 main = Blueprint('index_blueprint', __name__)
+
+
 @main.route('/')
 def index():
     try:
-        Logger.add_to_log("info", "Index/")
-        n = 1
-        print(n/0)
+        Logger.add_to_log("info", "{} {}".format(request.method, request.path))
+        return "Ok"
     except Exception as ex:
         Logger.add_to_log("error", str(ex))
         Logger.add_to_log("error", traceback.format_exc())
